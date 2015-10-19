91e613adf0901035ef42a8df88180cca391b55c3
vishfrnds <vishfrnds@gmail.com>
2015-10-20 02:12:19 +0530

    Refractered Code and Restucured Maven Framework

--- Response/src/main/java/phishDetect/GiveFinalRating.java
+++ Response/src/main/java/phishDetect/GiveFinalRating.java
@@ -7,17 +7,15 @@
 package phishDetect;
 
 import precompute.Data;
+
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
 
 /**
- *
  * @author vishwas
  */
 public class GiveFinalRating {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         int ans = 0;
         int l = Data.phish_list.size();
         //for(int i = 0; i < l; i +=100)
@@ -26,50 +24,45 @@ public class GiveFinalRating {
             //System.out.println("http://acade");
             ans += x;
         }
-       System.out.println(ans);
+        System.out.println(ans);
     }
-            
-    public int run(String _url)
-    {
-        
+
+    public int run(String _url) {
+
         _url = _url.replaceFirst("://www.", "://"); // remove www. from domain name      
         int match = URL_Utilities.perMatch(_url);
         System.out.println(_url + "\n" + match);
-        if(match >= 80)
+        if (match >= 80)
             return match;
         match = match / 5;    // 80 -> 8;
         match += URL_Utilities.checkURL(_url);
         System.out.println(_url + "\n" + match);
-        if(match > 105)
-            return match;           
-        match = match/3;   // 100->33
-        
+        if (match > 105)
+            return match;
+        match = match / 3;   // 100->33
+
         URL url = null;
-        try 
-        {
+        try {
             url = new URL(_url);
-        } 
-        catch (MalformedURLException ex) {
+        } catch (MalformedURLException ex) {
             ex.printStackTrace();
 
         }
-        if(url != null)
-        {
+        if (url != null) {
             Parse p = new Parse(_url);
             p.run();
-             System.out.println(_url+"\n NO doc");
-            if(p.getDoc() != null)
-            {
-                match = match/3;
-                 System.out.println(_url+"\n got doc");
+            System.out.println(_url + "\n NO doc");
+            if (p.getDoc() != null) {
+                match = match / 3;
+                System.out.println(_url + "\n got doc");
                 match += new HTML_Utilities().checkHTML(url, p.getDoc());
-                System.out.println(_url + "\n" + match);            
-                match += new Web_Utilities().checkWEB(_url);           
+                System.out.println(_url + "\n" + match);
+                match += new Web_Utilities().checkWEB(_url);
                 System.out.println(_url + "\n" + match);
             }
         }
         match /= 2;
         return match;
     }
-    
+
 }
